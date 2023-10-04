815057800f8c763651a17fff9012a03b18309522
Rasmus Krogsgaard <113165532+rasmuskrogsgaard@users.noreply.github.com>
2023-10-04 09:02:33 +0200

    jjj

--- firstgraphql/src/App.jsx
+++ firstgraphql/src/App.jsx
@@ -1,19 +1,18 @@
-import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
+import {QueryClient, QueryClientProvider, useQuery} from '@tanstack/react-query'
 import './App.css'
 import { Main } from './pages/Main'
-import React from 'react';
 import { Footer } from './footer/footer';
-const queryClient = new QueryClient()
 
+const queryClient = new QueryClient() 
 
 function App() {
 
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <Main></Main>
-      </QueryClientProvider>
-      <Footer />
+    <> 
+     <QueryClientProvider client={queryClient}>
+      <Main></Main>
+      <Footer></Footer>
+     </QueryClientProvider>
     </>
   )
 }
